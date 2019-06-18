15a9369a21600a8150f630ff75afa9dd66b01c05
Ubuntu <ubuntu@ip-172-26-6-242.ap-northeast-2.compute.internal>
2019-06-18 06:24:03 +0000

    Change baseurl, index page

--- custard/Server.cs
+++ custard/Server.cs
@@ -1,19 +1,29 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 using CreamRoll;
 using static CreamRoll.RouteServer;
 
 namespace custard {
     public class Server {
-        [Get("/")]
-        public async Task Index(RouteServerBase.RouteContext ctx) {
-            var file = File.OpenRead("Pages/index.html");
-            await file.CopyToAsync(ctx.Response.OutputStream);
+        private readonly string indexHtml;
+
+        public Server() {
+            indexHtml = File.ReadAllText("Pages/index.html");
         }
 
-        [Get("/api/v1/files/{name}", ManuallyResponse = true)]
+        [Get("/custard/", ContentType = "text/html; charset=utf8")]
+        public Task<string> Index(RouteServerBase.RouteContext ctx) {
+            return Task.FromResult(indexHtml.Replace("{{files}}",
+                            string.Join("<br>",
+                                                Directory.GetFiles("Files/")
+                                                                        .Select(s => s.Substring(6))
+                                                                                                .Select(s => $"<a href='/custard/api/v1/files/{s}'>{s}</a>"))));
+        }
+
+        [Get("/custard/api/v1/files/{name}", ManuallyResponse = true)]
         public async Task Upload(RouteServerBase.RouteContext ctx) {
             string path = Path.Combine("Files", ctx.Query.name);
             if (!File.Exists(path)) {
